4e7b0f8a80942ec66d7be328443b75fb4f33874e
Evgeniy Gorkavyi <e.gorkavyi@gmail.com>
2023-10-08 20:16:24 +0600

    Test is undone.

--- src/test/java/org/eg/tgbot/service/StatsServiceTest.java
+++ src/test/java/org/eg/tgbot/service/StatsServiceTest.java
@@ -4,13 +4,15 @@ import org.eg.tgbot.repository.StatsRepository;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StatsServiceTest {
@@ -36,14 +38,31 @@ class StatsServiceTest {
     void getStatsIncomesBetweenDatesTest() {
         Date from = Date.valueOf("2023-10-04");
         Date to = Date.valueOf("2023-10-05");
-        List<String> outputList = statsService.getStatsIncomesBetweenDates(from, to);
         List<String> expectedList = new ArrayList<>() {
             {
                 add("1300: 2023-10-04");
                 add("1509: 2023-10-05");
             }
         };
-        Assertions.assertLinesMatch(expectedList, outputList);
+
+        List<Map<String, Object>> returnList = new ArrayList<>();
+        returnList.add(new HashMap<>(){{
+            put("income", 1300);
+            put("date", Date.valueOf("2023-10-04"));
+        }});
+        returnList.add(new HashMap<>(){{
+            put("income", 1509);
+            put("date", Date.valueOf("2023-10-05"));
+        }});
+
+        Mockito.when(statsRepository.getStatsIncomesBetweenDates(
+                Date.valueOf("2023-10-04"),
+                Date.valueOf("2023-10-05")
+        ))
+                .thenReturn(returnList);
+        List<String> actualList = statsService.getStatsIncomesBetweenDates(from, to);
+
+        Assertions.assertLinesMatch(expectedList, actualList);
     }
 
     @DisplayName("Spends_Between_Dates_test")
@@ -51,12 +70,25 @@ class StatsServiceTest {
     void getStatsSpendsBetweenDatesTest() {
         Date from = Date.valueOf("2023-10-04");
         Date to = Date.valueOf("2023-10-05");
-        List<String> outputList = statsService.getStatsIncomesBetweenDates(from, to);
         List<String> expectedList = new ArrayList<>() {
             {
                 add("450: 2023-10-04");
             }
         };
-        Assertions.assertLinesMatch(expectedList, outputList);
+
+        List<Map<String, Object>> returnList = new ArrayList<>();
+        returnList.add(new HashMap<>(){{
+            put("spend", 450);
+            put("date", Date.valueOf("2023-10-04"));
+        }});
+
+        Mockito.when(statsRepository.getStatsSpendsBetweenDates(
+                        Date.valueOf("2023-10-04"),
+                        Date.valueOf("2023-10-05")
+                ))
+                .thenReturn(returnList);
+
+        List<String> actualList = statsService.getStatsSpendsBetweenDates(from, to);
+        Assertions.assertLinesMatch(expectedList, actualList);
     }
 }
